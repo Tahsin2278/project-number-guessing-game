649463e18af311bdaf13c5bbb1c5a8c5e0df4150
Tahsin2278 <nazmussaqibtahsin2007@gmail.com>
2026-02-19 12:52:24 -0800

    valid integer input testing and asking the user for an appropriate number range.

--- valid integer input .py
+++ valid integer input .py
@@ -14,6 +14,21 @@ def valid_integer(prompt: str) -> int:
         print("Please enter a valid integer.")
 
 
-def range():
-    min_num = valid_integer("Enter the minimum number: ")
-    max_num = valid_integer("Enter the maximum number: ")
+# Prompt the user for a low and high number to set the guessing range.
+"""
+It will ask the user to guess a number range and ensure higher number is greater than lower number.
+"""
+min_num = valid_integer("Enter the minimum number: ")
+max_num = valid_integer("Enter the maximum number: ")
+
+
+def get_range():
+    if min_num > max_num:
+        print("Please ensure the minimum number is lower than maximum number.")
+    elif max_num > min_num:
+        print("You chose appropriate number range.")
+    else:
+        print("You need to choose two different numbers.")
+
+
+number = get_range()
